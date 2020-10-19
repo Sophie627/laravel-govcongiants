3fdc64e97a7bef3e9383529a12d58355e9ed6fb4
Sophie627 <alekseizukovski@gmail.com>
2020-10-19 12:03:37 +0300

    Update UI/UX

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
+        
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
--- resources/views/data/show.blade.php
+++ resources/views/data/show.blade.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-tagsinput/1.3.6/jquery.tagsinput.min.css">
+<link rel="stylesheet" type="text/css" href="{{ url('/css/jquery.dropdown.css') }}" />
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-bold text-xl text-gray-800 leading-tight">
@@ -13,12 +15,15 @@
                         <div class="md:col-span-1 sm:col-span-3 pr-8">
                             <div class="py-3">
                                 <p class="font-black">Keywords</p>
-                                <textarea class="resize-none border border-gray-900 focus:border-blue-600 text-gray-900 focus:text-blue-600 w-full" rows="4"></textarea>
+                                <input class="resize-none border border-gray-900 focus:border-blue-600 text-gray-900 focus:text-blue-600 w-full" type="text" id="keywords">
                                 <p>Clear All</p>
                             </div>
                             <div>
                                 <p class="font-black">NAICS</p>
-                                <textarea class="resize-none border border-gray-900 focus:border-blue-600 text-gray-900 focus:text-blue-600 w-full" rows="4"></textarea>
+                                {{-- <input class="resize-none border border-gray-900 focus:border-blue-600 text-gray-900 focus:text-blue-600 w-full" type="text" id="naics"> --}}
+                                <div class="dropdown-mul-1 w-full">
+                                    <select style="display:none" name="" id="" multiple placeholder="Select"> </select>
+                                </div>
                                 <p>Clear All</p>
                             </div>
                         </div>
@@ -59,3 +64,82 @@
         </div>
     </div>
 </x-app-layout>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-tagsinput/1.3.6/jquery.tagsinput.min.js"></script>
+<script src="{{ url('/js/jquery.dropdown.js') }}"></script>
+<script type="text/javascript">
+    $(function() {
+        var json1 = {
+            data : [{
+                disable: true,
+                groupID: 2,
+                groupName: 'Group Name',
+                id: 1,
+                name: 'Joseph Mark Martin',
+                selected: false,
+            }, {
+                disable: true,
+                groupID: 2,
+                groupName: 'Group Name',
+                id: 2,
+                name: 'Joseph Mark Martin1',
+                selected: false,
+            }, {
+                disable: true,
+                groupID: 2,
+                groupName: 'Group Name',
+                id: 3,
+                name: 'Joseph Mark Martin2',
+                selected: false,
+            }, {
+                disable: true,
+                groupID: 2,
+                groupName: 'Group Name',
+                id: 4,
+                name: 'Joseph Mark Martin3',
+                selected: false,
+            }, ]
+        };
+        console.log(json1);
+        $('.dropdown-mul-1').dropdown({
+            data: json1.data,
+            limitCount: 40,
+            multipleMode: 'label',
+            choice: function() {
+                // console.log(arguments,this);
+            }
+        });
+        $('#keywords').tagsInput({
+            'height': '120px',
+            'width': '24vw',
+            'interactive': true,
+            'defaultText': '',
+            'onAddTag': function(tag) {
+                console.log("Added a tag: " + tag)
+            },
+            'onRemoveTag': function(tag) {
+                console.log("Removed a tag: " + tag);
+            },
+            'removeWithBackspace': true,
+            'minChars': 2,
+            'maxChars': 30,
+            'placeholderColor': '#777'
+        });
+        $('#naics').tagsInput({
+            'height': '120px',
+            'width': '24vw',
+            'interactive': true,
+            'defaultText': '',
+            'onAddTag': function(tag) {
+                console.log("Added a tag: " + tag)
+            },
+            'onRemoveTag': function(tag) {
+                console.log("Removed a tag: " + tag);
+            },
+            'removeWithBackspace': true,
+            'minChars': 2,
+            'maxChars': 30,
+            'placeholderColor': '#777'
+        });
+    });
+</script>
